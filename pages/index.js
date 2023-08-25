837d73a18f857aaa2c9e9a00ed22fb15be6cfa99
Gabriel Viggiani <136023068+gabrielviggianih@users.noreply.github.com>
2023-08-25 20:10:48 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home (){
-    return <h1>Obrigado por tudo feito até hoje por mim.</h1>
+    return <h1>Sandroca, obrigado por tudo que você fez até hoje por mim.</h1>
 }
 
 export default Home;
